fe97f6a960de8270245ef1544e6bcd51c5f8415a
Oleksandra Voichenko <ol.voichenko@gmail.com>
2024-09-17 23:03:15 +0300

    add collision logic

--- code/player.py
+++ code/player.py
@@ -1,12 +1,14 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('/home/oleksandra/drive/vampire-survivor/images/player/down/0.png').convert_alpha()
         self.rect = self.image.get_frect(center = pos)
         self.dir = pygame.Vector2()
         self.speed = 300
+        self.collision_sprites = collision_sprites
+        self.hitbox_rect = self.rect.inflate(-60, 0)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -15,7 +17,21 @@ class Player(pygame.sprite.Sprite):
         self.dir = self.dir.normalize() if self.dir else self.dir
 
     def move(self, dt):
-        self.rect.center += self.dir * self.speed * dt
+        self.hitbox_rect.x += self.dir.x * self.speed * dt
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.dir.y * self.speed * dt
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.dir.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.dir.x < 0: self.hitbox_rect.left = sprite.rect.right
+                if direction == 'vertical':
+                    if self.dir.y < 0: self.hitbox_rect.top = sprite.rect.bottom
+                    if self.dir.y > 0: self.hitbox_rect.bottom = sprite.rect.top
 
     def update(self, dt):
         self.input()
