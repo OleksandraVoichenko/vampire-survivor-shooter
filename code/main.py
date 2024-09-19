b3da0fa9409878e3c8bb403f0b7fc69ba4c57f2c
Oleksandra Voichenko <ol.voichenko@gmail.com>
2024-09-19 22:58:11 +0300

    finish gun logic and bullets

--- code/main.py
+++ code/main.py
@@ -15,7 +15,36 @@ class Game:
         self.player = None
         self.all_sprites = AllSprites()
         self.collisions_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
         self.setup()
+        self.load_images()
+
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+
+        self.enemy_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.enemy_event, 300)
+        self.spawn_pos = []
+
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('..', 'images', 'gun', 'bullet.png')).convert_alpha()
+
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_dir * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_dir, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            curr_time = pygame.time.get_ticks()
+            if curr_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
 
 
     def run(self):
@@ -25,6 +54,8 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
             self.screen.fill((0, 0, 0))
 
@@ -48,6 +79,7 @@ class Game:
         for obj in game_map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collisions_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
 
 if __name__ == '__main__':
--- code/sprites.py
+++ code/sprites.py
@@ -1,3 +1,7 @@
+from math import atan2, degrees
+import pygame.sprite
+from pygame.display import update
+
 from settings import *
 
 class Sprite(pygame.sprite.Sprite):
@@ -13,3 +17,68 @@ class CollisionSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        self.player = player
+        self.distance= 140
+        self.player_dir = pygame.Vector2(1, 0)
+
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('..', 'images', 'gun', 'gun.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center=self.player.rect.center + self.player_dir * self.distance)
+
+
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player_dir = (mouse_pos - player_pos).normalize()
+
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_dir.x, self.player_dir.y)) - 90
+        if self.player_dir.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
+
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_dir * self.distance
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, dir, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center=pos)
+        self.bullet_dir = dir
+        self.speed = 1200
+        self.spawn_time = pygame.time.get_ticks()
+        self.lifetime = 1000
+
+    def update(self, dt):
+        self.rect.center += self.bullet_dir * self.speed * dt
+        if pygame.time.get_ticks() - self.spawn_time >= 1000:
+            self.kill()
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos, frames, groups, player, collision_sprites):
+        super().__init__(groups)
+        self.player = player
+
+        self.frames, self.frame_index = frames, 0
+        self.images = self.frames[self.frame_index]
+        self.animation_speed = 6
+
+        self.rect = self.image.get_frect(center=pos)
+        self.hitbox_rect = self.rect.inflate(-20, -40)
+        self.collision_sprites = collision_sprites
+        self.enemy_dir = pygame.Vector(2)
+        self.speed = 300
